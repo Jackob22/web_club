d80abc6aa78d30ba8cf38aee63f54f0c4268fccb
bohdanst1914 <bohdanst19@gmail.com>
2021-09-07 00:13:04 +0200

    task 2

--- 2_task.js
+++ 2_task.js
@@ -1,16 +1,31 @@
 'use strict';
 
-const title  = document.querySelector('h1'),
-    blockDivItems = document.querySelector('#myDiv').children,
-    myList = document.querySelector('#myList'),
-    span = document.querySelector('span');
+class Person{
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    showFullName(){
+        console.log(`${this.name} ${this.surname}`)
+    }
+}
+class Student extends Person{
+    constructor(name,surname, year) {
+        super(name,surname);
+        this.year = year;
+    }
+    showFullName(middleName) {
+        return `${this.surname} ${this.name} ${middleName}`;
+    }
+    showCourse(){
+        const currentYear = new Date().getFullYear();
+        if (currentYear - this.year > 6)
+            return 6;
+        return currentYear - this.year;
 
-title.setAttribute('style', 'background: lime');
+    }
+}
 
-blockDivItems[0].setAttribute('style', 'font-weight: bold');
-blockDivItems[1].setAttribute('style', 'color: red');
-blockDivItems[2].style.textDecoration = 'underline';
-blockDivItems[3].style.fontStyle = 'italic';
-
-myList.style.cssText = 'display: flex; list-style-type: none';
-span.style.display = 'none';
+const stud1 = new Student('Petro', 'Petrenko', 2015)
+console.log(stud1.showFullName('Petrovych'));
+console.log('Current course: ' + stud1.showCourse());
